f714971bb1d7e972d87951b7b2ef65891bc08a82
713712 <wryx713@outlook.com>
2025-10-05 16:47:38 +0800

    0.6.5.5

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,11 @@ this.window = this;
 for (let i = 0; i < 8; i++) {
 	window["先驱者核心" + i] = new Block("先驱者核心" + i);
 };
-MapResizeDialog.minSize = 0
-MapResizeDialog.maxSize = 40000
+Vars.renderer.minZoom = 0.5,//最小缩放率(8/1)
+Vars.renderer.maxZoom = 8000000,//最大缩放率（8/1）（这个是有上限的，只是能让手机放大到电脑大小，并不能无上限）
+
+MapResizeDialog.minSize = 25,//最小地图大小
+MapResizeDialog.maxSize = 800000000,//最大地图大小
+
 require('开屏菜单');
 require('tank');
-//来自爬爬的模组
